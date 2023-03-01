20a6858d0fc28a66713d17d2525d4b3fd1138bd0
jacobread <jread8888@gmail.com>
2023-02-28 19:52:14 -0700

    Worked on problem 2 and I have the concept of it all implemented but I doubt it has no flaws. I also added two files that will be helpful for problem 3. Oh and I wrote my opinions on how Dr Mario can improve this class for problem 4.

--- Problem2.py
+++ Problem2.py
@@ -34,7 +34,7 @@ class Agent:
             return np.random.choice(np.flatnonzero(self.Q == self.Q.max()))
 
 
-def get_probabilities():
+def get_probabilities(withArea0):
     probs = [
         np.random.beta(7, 3) + 2,
         np.random.uniform(0, 4),
@@ -43,8 +43,35 @@ def get_probabilities():
         np.random.normal(1.3, 7),
     ]
 
+    # Now assume that you have a baseline sensor: area0 = normal(1.5, 3)
+    if withArea0:
+        probs = [
+            np.random.normal(1.5, 3),
+            np.random.beta(7, 3) + 2,
+            np.random.uniform(0, 4),
+            np.random.beta(3, 7) + 2,
+            np.random.normal(2, 1.4),
+            np.random.normal(1.3, 7),
+        ]
+        # Negative values set to zero
+        if probs[0] < 0:
+            probs[0] = 0
+
     return probs
 
+
+def get_area(num):
+    if num == 0:
+        return "area1"
+    elif num == 1:
+        return "area2"
+    elif num == 2:
+        return "area3"
+    elif num == 3:
+        return "area4"
+    elif num == 4:
+        return "area5"
+    
 
 # Start multi-armed bandit simulation
 def experiment(probs, N_episodes):
@@ -59,51 +86,65 @@ def experiment(probs, N_episodes):
         rewards.append(reward)
     return np.array(actions), np.array(rewards)
 
+
+# Plot reward results
+def plot_reward(output_dir):
+    R_avg =  R / float(N_steps)
+    plt.plot(R_avg, ".")
+    plt.xlabel("Step")
+    plt.ylabel("Average Reward")
+    plt.grid()
+    ax = plt.gca()
+    plt.xlim([1, N_steps])
+    if save_fig:
+        if not os.path.exists(output_dir): os.mkdir(output_dir)
+        plt.savefig(os.path.join(output_dir, "rewards.png"), bbox_inches="tight")
+    else:
+        plt.show()
+    plt.close()
+
+
+# Plot action results
+def plot_action(probs, actions, output_dir):
+    for i in range(len(probs)):
+        steps = list(np.array(range(len(actions)))+1)
+        plt.plot(steps, actions, ".",
+                linewidth=5,
+                label="Arm {}".format(i+1))
+    plt.xlabel("Step")
+    plt.ylabel("Selection over Time")
+    plt.xlim([1, N_steps])
+    plt.ylim([-2, 6])
+    if save_fig:
+        if not os.path.exists(output_dir): os.mkdir(output_dir)
+        plt.savefig(os.path.join(output_dir, "actions.png"), bbox_inches="tight")
+    else:
+        plt.show()
+    plt.close()
+
+
+def run(withArea0, output_dir, R):
+    for i in range(N_steps):
+        probs = get_probabilities(withArea0)
+        actions, rewards = experiment(probs, N_steps)  # perform experiment
+        R += rewards
+        for j, a in enumerate(actions):
+            A[j][a] += 1
+
+    print("Chosen Area: {}".format(get_area(np.argmax(np.bincount(actions)))))
+    plot_reward(output_dir)
+    plot_action(probs, actions, output_dir)
+
+
 N_steps = 500 # number of steps (episodes)
 eps = 0.1 # probability of random exploration (fraction)
 save_fig = True # save file in same directory
-output_dir = os.path.join(os.getcwd(), "output")
+output_dir1 = os.path.join(os.getcwd(), "output1")
+output_dir2 = os.path.join(os.getcwd(), "output2")
+R = np.zeros((N_steps)) # reward history sum
+A = np.zeros((N_steps, 21)) # action history sum
 
-
-R = np.zeros((N_steps,))  # reward history sum
-A = np.zeros((N_steps, 21))  # action history sum
-
-for i in range(N_steps):
-    probs = get_probabilities()
-    actions, rewards = experiment(probs, N_steps)  # perform experiment
-    R += rewards
-    for j, a in enumerate(actions):
-        A[j][a] += 1
-
-# Plot reward results
-R_avg =  R / float(N_steps)
-plt.plot(R_avg, ".")
-plt.xlabel("Step")
-plt.ylabel("Average Reward")
-plt.grid()
-ax = plt.gca()
-plt.xlim([1, N_steps])
-if save_fig:
-    if not os.path.exists(output_dir): os.mkdir(output_dir)
-    plt.savefig(os.path.join(output_dir, "rewards.png"), bbox_inches="tight")
-else:
-    plt.show()
-plt.close()
-
-# Plot action results
-for i in range(len(probs)):
-    steps = list(np.array(range(len(actions)))+1)
-    plt.plot(steps, actions, ".",
-             linewidth=5,
-             label="Arm {}".format(i+1))
-plt.xlabel("Step")
-plt.ylabel("Selection over Time")
-# leg = plt.legend(loc='upper left', shadow=True)
-plt.xlim([1, N_steps])
-plt.ylim([-2, 6])
-if save_fig:
-    if not os.path.exists(output_dir): os.mkdir(output_dir)
-    plt.savefig(os.path.join(output_dir, "actions.png"), bbox_inches="tight")
-else:
-    plt.show()
-plt.close()
+# Without area0
+run(False, output_dir1, R)
+# With area0
+run(True, output_dir2, R)
--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # Midterm
 
 ## Problem 1
-Start by solving the vanilla problem and then add the complexity (50%, etc) as you go
 
-### For just 50%:
+### For just 50% chance of accepting the job:
 I ran 10000 experiments and got 31% stopping point and 27% stopping point
 I ran 100000 experiments and got 34% stopping point
 
  How does this compare with the simple early-stopping problem?
  - The curve flattens out more
 
-### For 20% and increasing
+### For 20% and increasing chance of not accepting the job
 I ran 10000 experiments and got the following:
 - Optimal Search Threshold: 30.0%
 - Optimal Answer: 859
@@ -32,10 +31,19 @@ Optimal Answer: 560.6912085494506
 
 
 ## Problem 2
-The one that has the highest average is the one you should converge too
 
+### Without Baseline Sensor
 How many days will it take for you to make the decision to place the permanent sensor?
+
+
 Also, state the area that you select as the optimum.
+- Area1 was the selected optimum area.
+
+### With Baseline Sensor
+Does this change the final decision of placement?
+- No it does not.
+
+How long does it take before you choose to place the sensor?
 
 
 
@@ -44,6 +52,21 @@ Don't forget the price of the option is the current stock price - the time to ex
 
 
 
-## General Tips
-Dr. Mario said that if you cannot figure something out, experiment yourself and add a different complexity. Solve it,
-and explain it in either the readme or the code comments
+## Problem 4
+As this is the first time I have taught this topic, I could really use help in
+understanding how to improve the course. What is making sense, and what doesn’t?
+How can I do this better? 
+
+I think this course has a lot of potential. Where it is lacking, in my opinion, is in the early stages 
+of learning a new topic. I never felt confident, and honestly still don't, with the monte carlo or epsilon
+greedy algorithms. Maybe I am slow to pick up on this, but I felt like we went over it and coded it up once
+(which by the way was helpful) and then just assumed it was understood after that. If you payed me to go and 
+code up a simple implementation of these algorithms without existing code or google I wouldn't be able to do
+it. 
+
+On a different note, a document containing all the various financial terms mentioned would be extremely helpful.
+I realize that we can google terms and while that is true I find it very helpful to get the specific teacher's 
+definition of a term since they are the ones using it frequently. I would LOVE to understand all of the financial
+topics we have talked about better, and I wish I had heard of them before this class, but the reality is that I 
+had never heard of most of them. In short, a document that we could refer to for help with would be greatly
+appreciated.
